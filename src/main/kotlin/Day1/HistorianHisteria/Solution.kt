79edc5912d8e24c9649f59617646a58b34122f8c
jdelles <james.delles@gmail.com>
2024-12-03 11:07:27 -0500

    [2024 Day 1] Accepted solution to day 1, part 2

--- src/main/kotlin/Day1/HistorianHisteria/Solution.kt
+++ src/main/kotlin/Day1/HistorianHisteria/Solution.kt
@@ -4,7 +4,7 @@ import java.io.File
 import kotlin.math.abs
 
 class Solution {
-    private fun openFile(fileName: String): Pair<List<Int>, List<Int>> {
+    fun openFile(fileName: String): Pair<List<Int>, List<Int>> {
         val leftColumn = mutableListOf<Int>()
         val rightColumn = mutableListOf<Int>()
         File(fileName).forEachLine { line ->
@@ -19,19 +19,33 @@ class Solution {
         return Pair(leftColumn, rightColumn)
     }
 
-    fun processFile(fileName: String): Int {
-        val (list1, list2) = openFile(fileName)
-        val list1Sorted = list1.sorted()
-        val list2Sorted = list2.sorted()
-        val zipped = list1Sorted.zip(list2Sorted)
+    fun getDifference(lists: Pair<List<Int>, List<Int>>): Int {
+        val (left, right) = lists
+        val leftSorted = left.sorted()
+        val rightSorted = right.sorted()
+        val zipped = leftSorted.zip(rightSorted)
         val diff = zipped.map { (a, b) -> abs(b - a)}
         return diff.sum()
+    }
+
+    fun getSimilarityScore(lists: Pair<List<Int>, List<Int>>): Int {
+        val (left, right) = lists
+        val rightCount = right.groupingBy { it }.eachCount()
+        var similarityScore = 0
+        for (item in left) {
+            val count = rightCount[item] ?: 0
+            similarityScore += item * count
+        }
+        return similarityScore
     }
 }
 
 fun main() {
     val solution = Solution()
     val fileName = "src/main/kotlin/Day1/HistorianHisteria/input.txt"
-    val result = solution.processFile(fileName)
-    println("Result: $result")
+    val lists = solution.openFile(fileName)
+    val difference = solution.getDifference(lists)
+    println("Difference: $difference")
+    val similarityScore = solution.getSimilarityScore(lists)
+    println("Similarity Score: $similarityScore")
 }
