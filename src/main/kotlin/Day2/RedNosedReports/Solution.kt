bee5eb6ac3f66c6b37c2563e23a11f312131cc4a
jdelles <james.delles@gmail.com>
2024-12-03 15:49:03 -0500

    [2024 Day 2] Accepted solution to day 2

---
+++ src/main/kotlin/Day2/RedNosedReports/Solution.kt
@@ -0,0 +1,60 @@
+package Day2.RedNosedReports
+
+import java.io.File
+
+// https://adventofcode.com/2024/day/2
+
+class Solution {
+    fun openFile(fileName: String): List<List<Int>> {
+        val lists = mutableListOf<List<Int>>()
+        File(fileName).forEachLine { line ->
+            val list = mutableListOf<Int>()
+            val columns = line.split("\\s+".toRegex())
+            for (item in columns) {
+                val value = item.toIntOrNull()
+                value?.let { list.add(value) }
+            }
+            lists.add(list)
+        }
+        return lists
+    }
+
+    private fun isSafeReport(list: List<Int>): Boolean {
+        val increasing = list[1] > list[0]
+        for (i in 0..< list.size - 1) {
+            val diff = list[i + 1] - list[i]
+            if (diff !in 1..3 && diff !in -3..-1) {
+                return false
+            }
+            if (increasing != list[i + 1] > list[i]) {
+                return false
+            }
+        }
+        return true
+    }
+
+    private fun isSafeWithProblemDampener(list: List<Int>): Boolean {
+        for (i in list.indices) {
+            val dampenedList = list.filterIndexed { index, _ -> index != i }
+            if (isSafeReport(dampenedList)) return true
+        }
+        return false
+    }
+
+    fun countSafeReports(lists: List<List<Int>>, hasProblemDampener: Boolean = false): Int {
+        return lists.count { list ->
+            isSafeReport(list) || if (hasProblemDampener) isSafeWithProblemDampener(list) else false
+        }
+    }
+
+}
+
+fun main() {
+    val solution = Solution()
+    val input = "src/main/kotlin/Day2/RedNosedReports/input.txt"
+    val lists = solution.openFile(input)
+    val safeReportsCount = solution.countSafeReports(lists)
+    println("Count of safe reports: $safeReportsCount")
+    val safeReportsCountWithDampener = solution.countSafeReports(lists, true)
+    println("Count of safe reports with dampener: $safeReportsCountWithDampener")
+}
